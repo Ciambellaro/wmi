df7f3070d0389ac13c8107195be2be7f7e423fc4
Francesco Pezzulli <ipezzu@gmail.com>
2019-10-04 17:37:10 +0200

    Il server carica le pagine lautorizzazione di youtube

--- lienserver.js
+++ lienserver.js
@@ -1,29 +1,27 @@
 // Dependencies
+const Youtube = require("youtube-api"),
+    fs = require('fs'),
+    readJson = require("r-json"),
+    Lien = require("lien"),
+    Logger = require("bug-killer"),
+    opn = require("opn"),
+    prettyBytes = require("pretty-bytes");
 
-const Youtube = require("youtube-api")
-    , fs = require('fs')
-    , readJson = require("r-json")
-    , Lien = require("lien")
-    , Logger = require("bug-killer")
-    , opn = require("opn")
-    , prettyBytes = require("pretty-bytes")
-    ;
-
-    var express = require('express');
-    var app = express();
+var express = require('express');
+var app = express();
 
 // I downloaded the file from OAuth2 -> Download JSON
 const CREDENTIALS = readJson(`${__dirname}/credentials.json`);
 
 
-// Init lien server
-var server = new Lien({
-    host: "localhost"
-  , port: 8443
-  , public: __dirname + "/public"
-  , ssl: {
-        key: "privkey.pem"
-      , cert: "pubcert.pem"
+// Init lien app
+var app = new Lien({
+    host: "localhost",
+    port: 8443,
+    public: __dirname + "/public",
+    ssl: {
+        key: "privkey.pem",
+        cert: "pubcert.pem"
     }
 });
 
@@ -36,77 +34,80 @@ app.get('/map', function (req, res) {
 });*/
 
 // Listen for load
-server.on("load", function (err) {
-    console.log(err || "Server started on port 8443.");
+app.on("load", function(err) {
+    console.log(err || "server started on port 8443.");
     err && process.exit(1);
 });
 
-// Add page
-server.addPage("/", function (lien) {
-    lien.end("Hello World");
-});
-
-// Add a dynamic route
-server.addPage("/post/:id", function (lien) {
-    lien.end("Post id: " + lien.params.id);
-});
-
-server.addPage("/test", "/index.html");
-
 let oauth = Youtube.authenticate({
-  type: "oauth"
-, client_id: CREDENTIALS.web.client_id
-, client_secret: CREDENTIALS.web.client_secret
-, redirect_url: CREDENTIALS.web.redirect_uris[0]
+    type: "oauth",
+    client_id: CREDENTIALS.web.client_id,
+    client_secret: CREDENTIALS.web.client_secret,
+    redirect_url: CREDENTIALS.web.redirect_uris[0]
 });
 
 opn(oauth.generateAuthUrl({
-  access_type: "offline"
-, scope: ["https://www.googleapis.com/auth/youtube.upload"]
+    access_type: "offline",
+    scope: ["https://www.googleapis.com/auth/youtube.upload"]
 }));
 
+
+// Add page
+app.get("/", `${__dirname}/login.html`);
+
+app.get("/map", `${__dirname}/index.html`);
+
 // Handle oauth2 callback
-server.addPage("/oauth2callback", lien => {
-  Logger.log("Trying to get the token using the following code: " + lien.query.code);
-  oauth.getToken(lien.query.code, (err, tokens) => {
+app.get("/oauth2callback", lien => {
 
-      if (err) {
-          lien.lien(err, 400);
-          return Logger.log(err);
-      }
+    Logger.log("Trying to get the token using the following code: " + lien.query.code);
+    oauth.getToken(lien.query.code, (err, tokens) => {
 
-      Logger.log("Got the tokens.");
+        if (err) {
+            lien.lien(err, 400);
+            return Logger.log(err);
+        }
 
-      oauth.setCredentials(tokens);
+        Logger.log("Got the tokens.");
 
-      lien.end("The video is being uploaded. Check out the logs in the terminal.");
+        oauth.setCredentials(tokens);
 
-      var req = Youtube.videos.insert({
-          resource: {
-              // Video title and description
-              snippet: {
-                  title: "Prova pubblico"
-                , description: "Test video upload via YouTube API"
-              }
-              // I don't want to spam my subscribers
-            , status: {
-                  privacyStatus: "public"
-              }
-          }
-          // This is for the callback function
-        , part: "snippet,status"
+        lien.end("The video is being uploaded. Check out the logs in the terminal.");
 
-          // Create the readable stream to upload the video
-        , media: {
-              body: fs.createReadStream("video.mp4")
-          }
-      }, (err, data) => {
-          console.log("Done.");
-          process.exit();
-      });
+        var req = Youtube.videos.insert({
+            resource: {
+                // Video title and description
+                snippet: {
+                    title: "Prova pubblico",
+                    description: "Test video upload via YouTube API"
+                }
+                // I don't want to spam my subscribers
+                ,
+                status: {
+                    privacyStatus: "public"
+                }
+            }
+            // This is for the callback function
+            ,
+            part: "snippet,status"
 
-      setInterval(function () {
-          Logger.log(`${prettyBytes(req.req.connection._bytesDispatched)} bytes uploaded.`);
-      }, 250);
-  });
+                // Create the readable stream to upload the video
+                ,
+            media: {
+                body: fs.createReadStream("video.mp4")
+            }
+        }, (err, data) => {
+            console.log("Done.");
+            process.exit();
+        });
+
+        setInterval(function() {
+            Logger.log(`${prettyBytes(req.req.connection._bytesDispatched)} bytes uploaded.`);
+        }, 250);
+    });
+});
+
+// Listen for server errors
+app.on("serverError", err => {
+    console.log(err.stack);
 });
