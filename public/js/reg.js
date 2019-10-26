00d31d152d83c7f2200ff50268bd7f88a8f7bd40
Bassanelli Francesco <frebassa97@gmail.com>
2019-10-26 15:38:33 +0200

    Delete reg.js

--- public/js/reg.js
+++
@@ -1,6 +0,0 @@
-$("#btnRegistrati").on("click", function () {
-    $.post("/registrazione", function (data) {
-        var res = JSON.parse(data);
-        console.log("PRESO: " + res);
-    });
-})
